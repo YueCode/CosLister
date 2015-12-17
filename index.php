e3e7006c797b994acf93a75a50a4bd35d0172468
Bestony <best.tony@foxmail.com>
2015-12-17 18:08:43 +0800

    v1.1

--- index.php
+++ index.php
@@ -9,59 +9,21 @@ require 'Slim/Slim.php';
 \Slim\Slim::registerAutoloader();
 $app = new \Slim\Slim();
 
-// Get Directory List
 $app->get(
     '/',
     function () {
        echo "Hello World!";
     }
 );
-
 $app->get(
-	'/dirs',
-	function () {
-       $listRet = Cosapi::listFolder(BUCKET, "/");
-       $listRet	= $listRet['data'];
+	'/:dir',
+	function ($dir){
+	   $dir = str_replace('-','/',$dir);
+       $listRet = Cosapi::listFolder(BUCKET,$dir);
+	   $listRet	= $listRet['data'];
        $listRet	= json_encode($listRet);
        echo $listRet;
-    }
-);
-$app->get(
-	'/dir/:name',
-	function ($name){
-	    $listRet = Cosapi::listFolder(BUCKET,$name);
-	    $listRet	= $listRet['data'];
-        $listRet	= json_encode($listRet);
-        echo $listRet;
 	}
-);
-// POST route
-$app->post(
-    '/post',
-    function () {
-        echo 'This is a POST route';
-    }
-);
-
-// PUT route
-$app->put(
-    '/put',
-    function () {
-        echo 'This is a PUT route';
-    }
-);
-
-// PATCH route
-$app->patch('/patch', function () {
-    echo 'This is a PATCH route';
-});
-
-// DELETE route
-$app->delete(
-    '/delete',
-    function () {
-        echo 'This is a DELETE route';
-    }
 );
 
 $app->run();
